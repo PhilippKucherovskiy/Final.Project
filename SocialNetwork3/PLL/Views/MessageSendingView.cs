1a543efa3cdbbf7dd78f0b1b7f6bee522f7f5cb9
Philipp Kucherovskiy <philipp.kucherovskiy@gmail.com>
2023-04-22 01:50:08 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork3/PLL/Views/MessageSendingView.cs
@@ -0,0 +1,61 @@
+﻿using SocialNetwork3.BLL.Exceptions;
+using SocialNetwork3.BLL.Models;
+using SocialNetwork3.BLL.Services;
+using SocialNetwork3.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork3.PLL.Views
+{
+    public class MessageSendingView
+    {
+        UserService userService;
+        MessageService messageService;
+        public MessageSendingView(MessageService messageService, UserService userService)
+        {
+            this.messageService = messageService;
+            this.userService = userService;
+        }
+
+        public void Show(User user)
+        {
+            var messageSendingData = new MessageSendingData();
+
+            Console.Write("Введите почтовый адрес получателя: ");
+            messageSendingData.RecipientEmail = Console.ReadLine();
+
+            Console.WriteLine("Введите сообщение (не больше 5000 символов): ");
+            messageSendingData.Content = Console.ReadLine();
+
+            messageSendingData.SenderId = user.Id;
+
+            try
+            {
+                messageService.SendMessage(messageSendingData);
+
+                SuccessMessage.Show("Сообщение успешно отправлено!");
+
+                user = userService.FindById(user.Id);
+            }
+
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователь не найден!");
+            }
+
+            catch (ArgumentNullException)
+            {
+                AlertMessage.Show("Введите корректное значение!");
+            }
+
+            catch (Exception)
+            {
+                AlertMessage.Show("Произошла ошибка при отправке сообщения!");
+            }
+
+        }
+    }
+}
---
+++ SocialNetwork3/PLL/Views/UserDataUpdateView.cs
@@ -0,0 +1,42 @@
+﻿using SocialNetwork3.BLL.Models;
+using SocialNetwork3.BLL.Services;
+using SocialNetwork3.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork3.PLL.Views
+{
+    public class UserDataUpdateView
+    {
+        UserService userService;
+        public UserDataUpdateView(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public void Show(User user)
+        {
+            Console.Write("Меня зовут:");
+            user.FirstName = Console.ReadLine();
+
+            Console.Write("Моя фамилия:");
+            user.LastName = Console.ReadLine();
+
+            Console.Write("Ссылка на моё фото:");
+            user.Photo = Console.ReadLine();
+
+            Console.Write("Мой любимый фильм:");
+            user.FavoriteMovie = Console.ReadLine();
+
+            Console.Write("Моя любимая книга:");
+            user.FavoriteBook = Console.ReadLine();
+
+            this.userService.Update(user);
+
+            SuccessMessage.Show("Ваш профиль успешно обновлён!");
+        }
+    }
+}
